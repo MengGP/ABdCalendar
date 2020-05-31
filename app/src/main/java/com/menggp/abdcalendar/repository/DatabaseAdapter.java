92dfd8ef1942991c6dc252659cdb5706efa8992f
MengGP <oleg.ustsov@gmail.com>
2020-05-31 10:27:35 +0700

    upd: в классах DatabaseAdapter и SQLiteQueryHandler описана базовая CRUD логика работы с БД

--- app/src/main/java/com/menggp/abdcalendar/repository/DatabaseAdapter.java
+++ app/src/main/java/com/menggp/abdcalendar/repository/DatabaseAdapter.java
@@ -1,7 +1,16 @@
 package com.menggp.abdcalendar.repository;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+
+import com.menggp.abdcalendar.datamodel.Event;
+import com.menggp.abdcalendar.datamodel.EventAlertType;
+import com.menggp.abdcalendar.datamodel.EventType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
     Класс реализует адатпер работы с БД
@@ -16,6 +25,109 @@ public class DatabaseAdapter {
     public DatabaseAdapter(Context context) {
         dbHelper = new DatabaseHelper( context.getApplicationContext() );
     } // end_constructor
+
+    // Метод - открывает соеднение с БД на запись
+    public DatabaseAdapter open() {
+        db = dbHelper.getWritableDatabase();
+        return this;
+    } // end_method
+
+    // Метод закрывает соединение с БД
+    public void close() {
+        dbHelper.close();
+    } // end_method
+
+    // Метод возвращает записи EVENT в виде ArrayList
+    public List<Event> getEventsGeneral() {
+        ArrayList<Event> events = new ArrayList<>();    // ArrayList - для возврата результата метода
+        this.open();                                    // открываем соединение с БД
+
+        Cursor cursor = SQLiteQueryHandler.getAllEvents(db);
+
+        // Если курсор содержит данные - извлекаем их и помещаем в ArrayList - events
+        if (cursor.moveToFirst() ) {
+            do {
+                long id = cursor.getLong( cursor.getColumnIndex( dbHelper.COL_EVENT_ID) );
+                String eventName = cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_NAME) );
+                String eventDate = cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_DATE) );
+                EventType eventType = EventType.convertToEventType( cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_TYPE) ) );
+                int eventSinceYear = cursor.getInt( cursor.getColumnIndex(dbHelper.COL_EVENT_SINCE_YEAR) );
+                String eventComment = cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_COMMENT) );
+                int eventImg = cursor.getInt( cursor.getColumnIndex(dbHelper.COL_EVENT_IMG) );
+                EventAlertType eventAlertType = EventAlertType.convertToEventAlertType( cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_ALERT_TYPE) ) );
+
+                events.add( new Event(id, eventName, eventDate, eventType, eventSinceYear, eventComment, eventImg, eventAlertType) );
+            } while ( cursor.moveToNext() );
+        }
+        cursor.close();
+
+        this.close();                                   // закрываем соедиенение с БД
+        return events;
+    } // end_method
+
+     // Метод - возващает количество записей в БД
+    public long geEventCount() {
+        long count;
+        this.open();
+        count = DatabaseUtils.queryNumEntries(db, dbHelper.TABLE_EVENTS);
+        this.close();
+        return  count;
+    } // end_method
+
+    // Метод - возвращает объект по ID
+    public Event getEvent(long id) {
+        Event event = null;
+        this.open();
+
+        Cursor cursor = SQLiteQueryHandler.getEventByID(db, id);
+
+        // Если курсор содержит данные - извлекаем их
+        if (cursor.moveToFirst() ) {
+            // long id = cursor.getLong( cursor.getColumnIndex( dbHelper.COL_EVENT_ID) );   // ID - берем из входного аргумента
+            String eventName = cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_NAME) );
+            String eventDate = cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_DATE) );
+            EventType eventType = EventType.convertToEventType( cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_TYPE) ) );
+            int eventSinceYear = cursor.getInt( cursor.getColumnIndex(dbHelper.COL_EVENT_SINCE_YEAR) );
+            String eventComment = cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_COMMENT) );
+            int eventImg = cursor.getInt( cursor.getColumnIndex(dbHelper.COL_EVENT_IMG) );
+            EventAlertType eventAlertType = EventAlertType.convertToEventAlertType( cursor.getString( cursor.getColumnIndex(dbHelper.COL_EVENT_ALERT_TYPE) ) );
+
+            new Event(id, eventName, eventDate, eventType, eventSinceYear, eventComment, eventImg, eventAlertType);
+        }
+        cursor.close();
+
+        this.close();
+        return event;
+    } // end_method
+
+    // Метод - добавляет запись в таблицу EVENTS
+    public long insertEvent(Event event) {
+        long result;
+        this.open();
+        result = SQLiteQueryHandler.insertEvent(db, event);
+        this.close();
+        return result;
+    } // end_method
+
+    public long deleteEvent(long eventId) {
+        long result;
+        this.open();
+        result = SQLiteQueryHandler.deleteEvent(db, eventId);
+        this.close();
+        return  result;
+    } // end_method
+
+    // Метод обновляет запись в таблице EVENTS
+    public long update(Event event) {
+        long result;
+        this.open();
+        result = SQLiteQueryHandler.updateEvent(db, event);
+        this.close();
+        return result;
+    } // end_method
+
+
+
 
 
 
--- app/src/main/java/com/menggp/abdcalendar/repository/SQLiteQueryHandler.java
+++ app/src/main/java/com/menggp/abdcalendar/repository/SQLiteQueryHandler.java
@@ -1,10 +1,109 @@
 package com.menggp.abdcalendar.repository;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.menggp.abdcalendar.datamodel.Event;
+import com.menggp.abdcalendar.datamodel.EventAlertType;
+import com.menggp.abdcalendar.datamodel.EventType;
+
 /*
     Класс реализует взаимодействие с БД
         - описывает запросы к БД
  */
-public class SQLiteQueryHandler {
+class SQLiteQueryHandler {
+
+    // Метод возвращает все записи EVENT - в виде курсора
+    static Cursor getAllEvents(SQLiteDatabase db) {
+        // Массив строк - содержащеий получаемые в запросе поля
+        String[] columns = new String[] {
+                DatabaseHelper.COL_EVENT_ID,
+                DatabaseHelper.COL_EVENT_NAME,
+                DatabaseHelper.COL_EVENT_DATE,
+                DatabaseHelper.COL_EVENT_TYPE,
+                DatabaseHelper.COL_EVENT_SINCE_YEAR,
+                DatabaseHelper.COL_EVENT_COMMENT,
+                DatabaseHelper.COL_EVENT_IMG,
+                DatabaseHelper.COL_EVENT_ALERT_TYPE
+        };
+
+        return db.query(
+                DatabaseHelper.TABLE_EVENTS,        // целевая таблица
+                columns,                            // поля
+                null,                      // блок условий
+                null,                  // аргументы условий
+                null,                      // блок группировки
+                null,                       // блок HAVING
+                null                       // блок сортировки
+        );
+    } // end_method
+
+    // Метод возвращает запись EVENT по ID
+    static Cursor getEventByID(SQLiteDatabase db, long id) {
+        // Строка с описанием запроса
+        String query = String.format(
+                "SELECT * FROM %s WHERE %s=?",
+                DatabaseHelper.TABLE_EVENTS,
+                DatabaseHelper.COL_EVENT_ID
+        );
+
+        return db.rawQuery(
+                query,                              // запрос
+                new String[]{ String.valueOf(id) }  // массив строк - аргументов
+        );
+    } // end_method
+
+    // Метод добавляет в таблицу EVENTS запись
+    static long insertEvent(SQLiteDatabase db, Event event) {
+        // Создаем объект ContentValues - для передачи в БД
+        ContentValues cv = new ContentValues();
+        cv.put( DatabaseHelper.COL_EVENT_NAME, event.getEventName() );
+        cv.put( DatabaseHelper.COL_EVENT_DATE, event.getEventDate() );
+        cv.put( DatabaseHelper.COL_EVENT_TYPE, EventType.convertToString(event.getEventType()) );
+        cv.put( DatabaseHelper.COL_EVENT_SINCE_YEAR, event.getEventSinceYear() );
+        cv.put( DatabaseHelper.COL_EVENT_COMMENT, event.getEventComment() );
+        cv.put( DatabaseHelper.COL_EVENT_IMG, event.getEventImg() );
+        cv.put( DatabaseHelper.COL_EVENT_ALERT_TYPE, EventAlertType.convertToString(event.getEventAlertType()) );
+
+        return db.insert(
+                DatabaseHelper.TABLE_EVENTS,
+                null,
+                cv
+        );
+    } // end_class
+
+    // Метод удаляет запись из таблицы EVENTS
+    static long deleteEvent(SQLiteDatabase db, long eventId) {
+        String whereClause = DatabaseHelper.COL_EVENT_ID + " = ?";
+        String[] whereArgs = new String[] { String.valueOf(eventId) };
+        return db.delete(DatabaseHelper.TABLE_EVENTS, whereClause, whereArgs);
+    } // end_method
+
+    static long updateEvent(SQLiteDatabase db, Event event) {
+        String whereClause = DatabaseHelper.COL_EVENT_ID + "=" + String.valueOf( event.getId() );
+
+        // Создаем объект ContentValues - с данными для обновления
+        ContentValues cv = new ContentValues();
+        cv.put( DatabaseHelper.COL_EVENT_NAME, event.getEventName() );
+        cv.put( DatabaseHelper.COL_EVENT_DATE, event.getEventDate() );
+        cv.put( DatabaseHelper.COL_EVENT_TYPE, EventType.convertToString(event.getEventType()) );
+        cv.put( DatabaseHelper.COL_EVENT_SINCE_YEAR, event.getEventSinceYear() );
+        cv.put( DatabaseHelper.COL_EVENT_COMMENT, event.getEventComment() );
+        cv.put( DatabaseHelper.COL_EVENT_IMG, event.getEventImg() );
+        cv.put( DatabaseHelper.COL_EVENT_ALERT_TYPE, EventAlertType.convertToString(event.getEventAlertType()) );
+
+        return
+                db.update(
+                        DatabaseHelper.TABLE_EVENTS,    // таблица
+                        cv,                             // данные для обновления
+                        whereClause,                    // условие выбота обновляемой записи
+                        null                  // аргументы условия
+                );
+
+    } // end_method
+
+
 
 
 } // end_class
